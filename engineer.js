4e32d58fd07431b0c7a20a1cd9cd9d27888e6226
Mat Lundin <matmll12@gmail.com>
2021-10-16 22:07:16 -0500

    fixed some class parameters

--- engineer.js
+++ engineer.js
@@ -3,16 +3,16 @@ const Employee = require('./employee')
 class Engineer extends Employee{
   constructor(name, id, email, github){
     super(name, id, email)
-    this.name = name;
-    this.id = id;
-    this.email = email
-    github = github
+    // this.name = name;
+    // this.id = id;
+    // this.email = email
+    this.github = github
   }
   getRole(){
     return 'Engineer'
   }
   getGithub(){
-    return `https://github.com/${github}`
+    return `https://github.com/${this.github}`
     
   }
 }
--- intern.js
+++ intern.js
@@ -3,10 +3,10 @@ const Employee = require("./employee");
 class Intern extends Employee(){
     constructor(name, id, email, school){
         super(name, id, email)
-        this.name = name
-        this.id = id
-        this.email = email
-        school = school
+        // this.name = name
+        // this.id = id
+        // this.email = email
+        this.school = school
     }
     getRole(){
         return 'Intern'
